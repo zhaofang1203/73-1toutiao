18234fa6384ad62531a2e344d463ac195cfda1d9
zhaofang1203 <752743406@qq.com>
2019-07-09 21:07:23 +0800

    router element-ui

---
+++ src/router/router.js
@@ -0,0 +1,10 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import ElentUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElentUI)
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+export default router
